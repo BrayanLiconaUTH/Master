0e1c96778b34594ac8f64dc43be048abad7492db
PalaciosUTH <yoni_palacios@hotmail.com>
2018-07-18 00:19:59 -0500

    Parte de Sub clase Estudiante Grupal

---
+++ Estudiante1/src/estudiante1/Estudiante1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estudiante1;
+
+/**
+ *
+ * @author ADMIN
+ */
+ public static class Estudiante extends Personas {
+
+        public Estudiante() {
+            setNombre("Juan Enrique Bonilla Diaz");
+
+            setCodigo("201410120134");
+
+            setCarrera("INGENIERIA EN COMPUTACION");
+
+            setPeriodoest(2);
+
+            setindice(93);
+        }
+
+
+        public String getPersonasInformacion() {
+            return "Informacion Del Estudiante : Juan Enrique Bonilla Diaz ";
+        }
+
+
+    }
