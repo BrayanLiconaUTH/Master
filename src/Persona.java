88a7934c983df811740d3091750b12359c7020c2
aamaldonado7 <almaldonado78@hotmail.com>
2018-07-17 21:50:40 -0600

    Super clase Persona corregida

--- src/Persona.java
+++ src/Persona.java
@@ -11,6 +11,8 @@
 
   public class Persona {
 
+   
+
     public static void main(String arg[]) {
        
         System.out.println("-> Estudiante ");
@@ -22,7 +24,88 @@
         ada.imprimirInformacion();
     }
 
-  }
+    public static abstract class Personas {
+        private String nombre;
+        private String codigo;
+        private String carrera;
+        private int periodoest;
+        private int indice;
+
+        public Personas() {
+     
+        }
+        public Personas(String nombre,
+                          String codigo,
+                          String carrera,
+                          int periodoest,
+                          int indice) {
+            this.nombre = nombre;
+
+            this.codigo = codigo;
+
+            this.carrera = carrera;
+
+            this.periodoest = periodoest;
+
+            this.indice = indice;
+        }
+        public String getNombre() {
+            return this.nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getCodigo() {
+            return this.codigo;
+        }
+
+        public void setCodigo(String codigo) {
+            this.codigo = codigo;
+        }
+        public String getCarrera() {
+            return this.carrera;
+        }
+
+        public void setCarrera(String carrera) {
+            this.carrera = carrera;
+        }
+        public int getPeriodoest() {
+            return this.periodoest;
+        }
+
+        public void setPeriodoest(int periodoest) {
+            this.periodoest = periodoest;
+        }
+        public int getindice() {
+            return this.indice;
+        }
+
+        public void setindice(int indice) {
+            this.indice = indice;
+        }
+
+        public void imprimirInformacion() {
+            System.out.println("");
+
+            System.out.println("Nombre: " + nombre);
+
+            System.out.println("Codigo: " + codigo);
+
+            System.out.println("Carrera: " + carrera);
+
+            System.out.println("Año De Estudio: " + periodoest);
+
+            System.out.println("Indice: " + indice);
+
+            System.out.println("Informacion: " + getPersonasInformacion());
+
+            System.out.println("");
+        }
+    
+    } abstract String getPersonasInformacion();
+
+    }
     
 
   
